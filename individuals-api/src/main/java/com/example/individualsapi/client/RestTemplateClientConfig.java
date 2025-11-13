8b15add27c7663bb1df8c2ea948fa48ab581e229
WoldemarK <kovtynov.vladimir@gmail.com>
2025-11-13 19:09:08 +0300

    NEXUS подключение

--- individuals-api/src/main/java/com/example/individualsapi/client/RestTemplateClientConfig.java
+++ individuals-api/src/main/java/com/example/individualsapi/client/RestTemplateClientConfig.java
@@ -5,17 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-
 @Configuration
 public class RestTemplateClientConfig {
 
     @Bean
     public RestTemplate webClient(RestTemplateBuilder builder) {
-        return builder
-                .connectTimeout(Duration.ofSeconds(10))
-                .readTimeout(Duration.ofSeconds(10))
-                .build();
+        return builder.build();
     }
 
 }
